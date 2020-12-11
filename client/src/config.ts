6f3bc1948a536a359cacce82b1faff9dcb7aa116
Vijval Rajan <rajan-vijval@users.noreply.github.com>
2020-12-10 19:33:17 -0500

    Add files via upload

---
+++ client/src/config.ts
@@ -0,0 +1,10 @@
+
+const apiId = '2z9sdic1hj'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  
+  domain: 'dev-tk2m75mz.auth0.com',            // Auth0 domain
+  clientId: '0Z4ZocBelgfq4S85jyOJTxVv10h1wmr1',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
